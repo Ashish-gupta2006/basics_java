6b17466a6e33455ac2042647b6bd30df025d7b73
Ashish-gupta2006 <ashishgupta16082@gmail.com>
2024-08-31 04:33:20 +0000

    Add files via upload

---
+++ Firstprogram.java
@@ -0,0 +1,10 @@
+/**
+ * Firstprogram in java lanruage and started to in 3rd semstar
+ */
+public class Firstprogram {
+
+    public static void main(String[] args) {
+        System.out.println("my first program in java (3rd sem)");
+        System.out.println("hello word");
+    }
+}
